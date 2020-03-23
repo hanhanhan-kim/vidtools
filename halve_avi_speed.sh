bcaea7834530712a9e640862b28fe4926e9a9b05
hanhanhan-kim <hankim@caltech.edu>
2020-03-22 21:50:37 -0700

    Add a reference as comment

--- halve_avi_speed.sh
+++ halve_avi_speed.sh
@@ -2,6 +2,9 @@
 # Videos captured at faster frame rates need to be converted to a slower rate,
 # then converted to a faster rate afterwards. 
 # This script speeds or slow down compressed avis by a desired factor. 
+
+# See https://askubuntu.com/questions/754316/how-can-the-speed-of-an-avi-file-be-changed as reference
+
 ffmpeg -i *_converted.avi \
        -filter:v "setpts=0.5*PTS" \
        -c:v mpeg4 -q:v 1 \
