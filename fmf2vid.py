a4b8677f547a407f3d70014e80f0c6e424ca30c1
hanhanhan-kim <hankim@caltech.edu>
2020-04-01 21:09:37 -0700

    Combine 2avi and 2mp4 fxns

--- fmf2vid.py
+++ fmf2vid.py
@@ -13,6 +13,7 @@ import shutil
 import glob
 import sys
 import argparse
+from os.path import join
 
 import motmot.FlyMovieFormat.FlyMovieFormat as FMF
 from ffmpy import FFmpeg
@@ -28,7 +29,7 @@ def mkdirs4tiffs (names):
     
     Returns:
     Empty directories for each .fmf video. Can undo in terminal by navigating \
-    to `vid_path` and executing `rm -r */` 
+    to video path and executing `rm -r */` 
     '''
     
     assert (names),\
@@ -103,7 +104,8 @@ def fmf2tiff (names):
     names (list): a list of the .fmf files to be converted
     
     Returns:
-    A directory of .tiff files, where each directory corresponds to an .fmf object. Can undo in terminal by navigating to `vid_path` and executing `rm -r */`
+    A directory of .tiff files, where each directory corresponds to an .fmf object.\
+    Can undo in terminal by navigating to `vid_path` and executing `rm -r */`
     
     '''
 
@@ -132,17 +134,18 @@ def fmf2tiff (names):
             i += 1
 
 
-def tiff2mp4 (names, save_tiffs, crf):
+def tiff2vid (names, output, save_tiffs, crf):
     
     '''
     Converts .tiffs located in a directory into an .mp4 file.\
     Can batch process multiple directories of .tiffs into multiple respective .mp4 files.
     
     Paramters:
-    names (list): a list of the .fmf files to be converted.
-    save_tiffs (str): a flag to delete directories containing the input tiff\
+    names (list): A list of the .fmf files to be converted.
+    output (str): The desired output video type. Accepts either "avi" or "mp4".
+    save_tiffs (str): A flag to delete directories containing the input tiff\
     files, after conversion
-    crf (int): the constant rate factor for video compression, ranging from 0,\
+    crf (int): The constant rate factor for video compression, ranging from 0,\
      the least compressed, to 51, the most compressed. 
     
     Returns:
@@ -156,6 +159,9 @@ def tiff2mp4 (names, save_tiffs, crf):
     for name in names:
         assert(".fmf" in name),\
             f"The file {name} is not an .fmf video. Please provide an .fmf file."
+    assert (output == "avi" or "mp4"), \
+        f"Please specify either 'avi' or 'mp4', and not {output} as the output\
+        file type."
     
     in_paths = []
     out_paths = []
@@ -172,19 +178,35 @@ def tiff2mp4 (names, save_tiffs, crf):
         timeLen = tmStmps[-1] - tmStmps[0]
         frameRate = vidSize/timeLen
         
-        # FFmpeg conversions
-        in_paths.append(name.replace('.fmf','/%08d.tiff'))
-        out_paths.append(name.replace('.fmf','.mp4'))
-        
-        ff = FFmpeg(
-            inputs={in_paths[names.index(name)]: '-r '+ 
-                    str(frameRate) +
-                    ' -f image2'},
-            outputs={out_paths[names.index(name)]: '-crf ' + 
-                    str(crf) + 
-                    ' -pix_fmt yuv420p'} 
-        )
-        ff.run()
+        if output == "avi":
+            # Convert:
+            in_paths.append(name.replace('.fmf','/%08d.tiff'))
+            out_paths.append(name.replace('.fmf','.avi'))
+            
+            ff = FFmpeg(
+                inputs={in_paths[names.index(name)]: '-r '+ 
+                        str(frameRate) +
+                        ' -f image2'},
+                outputs={out_paths[names.index(name)]: '-c:v libx264 -crf ' + 
+                        str(crf) + 
+                        ' -pix_fmt yuv420p'} 
+            )
+            ff.run()
+
+        elif output == "mp4":
+            # Convert:
+            in_paths.append(name.replace('.fmf','/%08d.tiff'))
+            out_paths.append(name.replace('.fmf','.mp4'))
+            
+            ff = FFmpeg(
+                inputs={in_paths[names.index(name)]: '-r '+ 
+                        str(frameRate) +
+                        ' -f image2'},
+                outputs={out_paths[names.index(name)]: '-crf ' + 
+                        str(crf) + 
+                        ' -pix_fmt yuv420p'} 
+            )
+            ff.run()
 
         # Remove folders containing tiffs, based on flag:
         if save_tiffs == "false":
@@ -202,83 +224,17 @@ def tiff2mp4 (names, save_tiffs, crf):
             continue
 
 
-def tiff2avi (names, save_tiffs, crf):
-    
-    '''
-    Converts .tiffs located in a directory into an .avi file. Can batch process\
-     multiple directories of .tiffs into multiple respective .avi files.
-    
-    Paramters:
-    names (list): a list of the .fmf files to be converted.
-    save_tiffs (str): a flag to delete directories containing the input tiff\
-     files, after conversion
-    crf (int): the constant rate factor for video compression, ranging from 0,\
-     the least compressed, to 51, the most compressed. 
-    
-    Returns:
-    .avi files in the same directory as the .fmf files. Can undo in terminal by \
-    navigating to `vid_path` and executing `rm *.!(fmf)` 
-    
-    '''
-    
-    assert (0 <= crf <= 51), "crf is not an int between 0 and 51."
-    assert (names),\
-        "You've inputted an empty list. Please provide a populated list."
-    for name in names:
-        assert(".fmf" in name),\
-            f"The file {name} is not an .fmf video. Please provide an .fmf file."
-
-    in_paths = []
-    out_paths = []
-    
-    fmfs = []
-    
-    for name in names:
-        
-        # Compute the exact frame rate of each video to input into the FFmpeg conversions
-        fmfs.append(FMF.FlyMovie(name))
-        fmf = fmfs[names.index(name)]
-        vidSize = fmf.get_n_frames()
-        tmStmps = fmf.get_all_timestamps()
-        timeLen = tmStmps[-1] - tmStmps[0]
-        frameRate = vidSize/timeLen
-        
-        # FFmpeg conversions
-        in_paths.append(name.replace('.fmf','/%08d.tiff'))
-        out_paths.append(name.replace('.fmf','.avi'))
-        
-        ff = FFmpeg(
-            inputs={in_paths[names.index(name)]: '-r '+ 
-                    str(frameRate) +
-                    ' -f image2'},
-            outputs={out_paths[names.index(name)]: '-c:v libx264 -crf ' + 
-                    str(crf) + 
-                    ' -pix_fmt yuv420p'} 
-        )
-        ff.run()
-
-        # Remove folders containing tiffs, based on flag:
-        if save_tiffs == "false":
-
-            os.chdir(name.replace(".fmf",""))
-
-            if all (glob.glob(name.replace(".fmf",".tiff"))) is True:
-                shutil.rmtree(name.replace(".fmf",""))
-                os.chdir("..")
-            else:
-                print("No .tiff files in this directory")
-        
-        else:
-            print("Remove .tiffs flag is false")
-            continue
-
-
 def main():
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("vid_path",
-        help="Absolute path to the directory that houses the .fmf videos\
+    parser.add_argument("root",
+        help="Absolute path to the root directory. I.e. the outermost folder\
+            that houses the .fmf video files.\
             E.g.'/mnt/2TB/data_in/vids/'")
+    parser.add_argument("nesting", type=int,
+        help="Specifies the number of folders that are nested from the root\
+            directory. I.e. the number of folders between root and the subdirectory\
+            that houses the .fmf video files.")
     parser.add_argument("output_type", nargs="?", default="avi",
         help="The compressed video output type. Accepts either 'avi' or 'mp4'.\
             Default is 'avi'")
@@ -291,30 +247,23 @@ def main():
             the .fmf file is retained. Default is false.")
     args = parser.parse_args()
 
-    vid_path = args.vid_path
+    root = args.root
+    nesting = args.nesting
     output_type = args.output_type
     crf = args.crf
     save_tiffs = args.tiffs
         
     # Get the list of .fmf files to be converted:
-    if vid_path.endswith("/"):
-        names = sorted(glob.glob(vid_path + "*.fmf"))
-    else:
-        names = sorted(glob.glob(vid_path + "/*.fmf"))
+    names = sorted(glob.glob(join(root, nesting * "*/", "*.fmf")))
 
     assert (names), \
-        f"The directory, {vid_path}, is empty. Are you sure you specified a directory?"
+        f"The directory, {root}, is empty. Are you sure you specified a directory?"
 
     # Convert:
     mkdirs4tiffs(names)
     get_framerate_duration(names)
     fmf2tiff(names)
-
-    # Convert to either .mp4 or .avi:
-    if output_type == "mp4":
-        tiff2mp4(names, save_tiffs, crf)
-    elif output_type == "avi":
-        tiff2avi(names, save_tiffs, crf)
+    tiff2vid(names, output_type, save_tiffs, crf)
 
 
 if __name__ == "__main__":
