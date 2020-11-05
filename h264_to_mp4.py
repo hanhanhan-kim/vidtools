91f1b1c7b70709ed5bcf24553e85cdf5e22458a6
Han Kim <hankim@caltech.edu>
2020-11-05 10:01:32 -0800

    rm mono flag option

--- h264_to_mp4.py
+++ h264_to_mp4.py
@@ -2,7 +2,7 @@
 
 """
 Batch convert .h264 files to .mp4.
-Can output an additional monochrome .mp4. 
+Can output the .mp4 file in monochrome.  
 """
 
 import subprocess
@@ -18,40 +18,22 @@ def main():
         help="Absolute path to the root directory.")
     parser.add_argument("framerate", nargs="?", default=30,
         help="Framerate (int)")
-    parser.add_argument("-m", "--mono", action="store_true",
-        help="Convert to monochrome")
     args = parser.parse_args()
 
     root = expanduser(args.root)
     framerate = str(args.framerate)
-    mono = args.mono
     vids = sorted(glob.glob(join(root, "*.h264")))
 
     for vid in vids:
 
         output_vid = f"{splitext(vid)[0]}.mp4"
-
+            
         # Convert:
-        args = ["ffmpeg", "-framerate", framerate, "-i", vid, "-c", "copy", output_vid]
-        equivalent_cmd = f"ffmpeg -framerate {framerate} -i {vid} -c copy {output_vid}"
+        args = ["ffmpeg", "-framerate", framerate, "-i", vid, "-vf", "format=gray output", "-c", "copy", output_vid]
+        equivalent_cmd = f"ffmpeg -framerate {framerate} -i {vid} -vf format=gray output -c copy {output_vid}"
 
         print(f"running command {equivalent_cmd} from dir {root}")
         subprocess.run(args, cwd=root)
-
-        if mono:
-            
-            # TODO: Find better way, this command tanks the vid quality: 
-
-            mono_vid = f"{splitext(vid)[0]}_mono.mp4"
-
-            args = ["ffmpeg", "-i", output_vid, "-vf", "hue=s=0", mono_vid]
-            equivalent_cmd = f"ffmpeg -i {output_vid} -vf hue=s=0 {mono_vid}"
-
-            print(f"running command {equivalent_cmd} from dir {root}")
-            subprocess.run(args, cwd=root) 
-
-        else:
-            pass
 
 if __name__ == "__main__":
     main()
