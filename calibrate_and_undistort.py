476c620ef89e8f0dea7a1eebe0c734e5ea4e265c
Han Kim <hankim@caltech.edu>
2021-04-26 00:34:40 -0700

    tweak main docstring

--- calibrate_and_undistort.py
+++ calibrate_and_undistort.py
@@ -480,7 +480,7 @@ def main():
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("board_vid", 
         help="Path to the input calibration video of the checkerboard.\n" 
-            "Must NOT be called 'checkerboards', and be an .mp4 or a folder \n" 
+            "Must NOT be called 'checkerboards'. Must be an .mp4 or a folder \n" 
             "containing .jpgs. If a .pkl file for the calibration exists, \n" 
             "it should be in the same directory that `board_vid` is in. \n")
     parser.add_argument("framerate",
