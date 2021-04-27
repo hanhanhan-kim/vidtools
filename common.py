8ada28bcad562829352e9b035453cb727d3d5d8b
Han Kim <hankim@caltech.edu>
2021-04-26 18:09:20 -0700

    add common fxns (not a script)

---
+++ common.py
@@ -0,0 +1,42 @@
+"""
+Utility functions for helping with video analyses. 
+"""
+
+def ask_yes_no(question, default="yes"):
+
+    """
+    Ask a yes/no question and return the answer.
+
+    Parameters:
+    -----------
+    question (str): The question to ask the user. 
+    default: The presumed answer if the user hits only <Enter>. 
+        Can be either "yes", "no", or None. Default is "yes".
+
+    Returns:
+    ---------
+    bool
+    """
+
+    valid = {"yes": True, "y": True,
+             "no": False, "n": False}
+
+    if default is None:
+        prompt = "[y/n]\n"
+    elif default == "yes":
+        prompt = "[Y/n]\n"
+    elif default == "no":
+        prompt = "[y/N]\n"
+    else:
+        raise ValueError(f"invalid default answer: {default}")
+
+    while True:
+
+        print(f"{question} {prompt}")
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print("Please respond with 'yes'/'y' or 'no'/'n'. \n")
